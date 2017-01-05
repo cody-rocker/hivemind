217749c1299184b4a3979ead23dfa93944336573
Cody Rocker <codiferus17@gmail.com>
2017-01-05 00:28:05 -0500

    remove deprecated code

--- ext/js/popup.js
+++ ext/js/popup.js
@@ -61,28 +61,6 @@ function HiddenPost(obj) {
     return this.container;
 }
 
-// // Build a hiddenPost div object
-// // TODO: can this be prototyped?
-// function hiddenPost(obj) {
-//     var targetTab = "New Tab"; //TODO: make or link this to a configurable opt
-//     var _div = document.createElement('div'),
-//         _title = document.createElement('p'),
-//         _img = document.createElement('img'),
-//         _linkUrl = document.createElement('span');
-//     _div.id = obj.id;
-//     _img.src = obj.imgUrl;
-//     _img.style.display = "none";  //TODO: make it an opt to hide by default or not.
-//     _img.title = "Click to open in " + targetTab;
-//     _linkUrl.className = "hide";
-//     _linkUrl.appendChild(document.createTextNode(obj.postUrl));
-//     _title.appendChild(document.createTextNode(obj.title));
-//     _div.appendChild(_title);
-//     _div.appendChild(_img);
-//     _div.appendChild(_linkUrl);
-//     _div.className = "post shadow";
-//     return _div;
-// }
-
 // Add hidden posts to popup page
 function showHidden(blocklist) {
     // Update status text
@@ -91,7 +69,6 @@ function showHidden(blocklist) {
     // Add new divs to DOM
     var blockedPosts = document.getElementById('blocked-posts');
     for ( i in blocklist ) {
-        // blockedPosts.appendChild(hiddenPost(blocklist[i]));
         blockedPosts.appendChild(new HiddenPost(blocklist[i]));
     }
 }
