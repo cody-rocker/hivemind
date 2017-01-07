22cc743aa1ca711ddf4775aaa18572dfbe6997d0
Cody Rocker <codiferus17@gmail.com>
2017-01-06 23:31:47 -0500

    move social links to xml/javascript

--- ext/js/options.js
+++ ext/js/options.js
@@ -2,8 +2,6 @@
 var backgroundPage = chrome.extension.getBackgroundPage();
 var storage = chrome.storage.local;
 
-
-
 function populateTabs() {
     $.get('option-general.html', function(data) {
         $('#options-general').append(data);
@@ -11,6 +9,42 @@ function populateTabs() {
     $.get('option-filters.html', function(data) {
         $('#options-filters').append(data);
     });
+}
+
+function SocialLink(xmlElement) {
+    var _link = document.createElement('a');
+    var _icon = document.createElement('span');
+
+    // Parse xml element
+    this.name = xmlElement.getAttribute('Name');
+    this.url = xmlElement.getAttribute('Url');
+    this.icon = xmlElement.getAttribute('Icon');
+
+    // Create html element
+    _link.href = this.url;
+    _link.dataset.toggle = 'tooltip';
+    _link.dataset.placement = 'bottom';
+    _icon.className = this.icon;
+    _icon.title = this.name;
+    _icon.setAttribute('aria-hidden', true);
+    _link.appendChild(_icon);
+
+    this.li = document.createElement('li');
+    this.li.className = 'social';
+    this.li.appendChild(_link);
+    return this.li;
+}
+
+function displaySocialLinks() {
+    var parser = new DOMParser();
+    $.get('social-metadata.xml', function(data) {
+        var xmlDoc = parser.parseFromString(data, 'text/xml');
+        var socialLinks =  xmlDoc.getElementsByTagName('Link');
+        for (i = 0; i < socialLinks.length; i++ ) {
+            $('#social-links').append(new SocialLink(socialLinks[i]));
+        }
+    });
+
 }
 
 function displayVersionNumber() {
@@ -32,5 +66,6 @@ $('#tabs').tabs();
 $(document).ready(function () {
     // misc. page setup
     populateTabs();
+    displaySocialLinks();
     displayVersionNumber();
 });
