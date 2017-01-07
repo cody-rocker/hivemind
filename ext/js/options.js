89c8351f3f28fcead2ff29efabb3cc8339d31ddf
Cody Rocker <codiferus17@gmail.com>
2017-01-07 13:27:29 -0500

    keep dialing in css layout

--- ext/js/options.js
+++ ext/js/options.js
@@ -77,6 +77,7 @@ $(document).ready(function () {
     $('#tabs').tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
     $("#tabs li").removeClass('ui-corner-top').addClass('ui-corner-left');
     $('ul.ui-tabs-nav').addClass('shadow');
+    // Setup options page
     populateTabs();
     linkToHome();
     displaySocialLinks();
